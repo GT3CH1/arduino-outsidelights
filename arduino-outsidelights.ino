f68d913108ce35f9319b94383c8d51da7ba8bfe0
Gavin Pease <gavinpease@gmail.com>
2021-05-01 17:00:51 -0600

    Update arduio code to match firebase

--- arduino-outsidelights.ino
+++ arduino-outsidelights.ino
@@ -2,19 +2,16 @@
 #include "secret.h"
 
 const int lightPin = 2;
-const int pumpPin = 3;
 const int resetPin = 4;
-int invertedPins[] = {3};
+int invertedPins[] = {};
 GoogleArduino automation(resetPin);
 
 void setup(){
   automation.setup(FIREBASE_HOST,FIREBASE_AUTH,WIFI_SSID,WIFI_PASSWORD,invertedPins);
-  automation.firstRun("fishtank-light",lightPin);
-  automation.firstRun("fishtank-pump",pumpPin);
+  automation.firstRun("1",lightPin);
 }
 
 void loop(){
-  automation.checkStatus("fishtank-light",lightPin);
-  automation.checkStatus("fishtank-pump",pumpPin);
+  automation.checkStatus("1",lightPin);
   delay(5000);
 }
